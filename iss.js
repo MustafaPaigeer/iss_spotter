45a3d0a40eef8179be8177074a2034a81356725c
Mustafa Paigeer <paigeer2012@gmail.com>
2022-03-14 04:44:58 +0000

    Implement fetchMyIP function

--- iss.js
+++ iss.js
@@ -3,7 +3,15 @@ const request = require('request');
 const fetchMyIP = function(callback) {
     // use request to fetch IP address from JSON API
     request(`https://api.ipify.org?format=json`, (error, res, body) => {
+      if (error) return callback("There has been an error retieving IP address: " + error, null);
 
+      if (res.statusCode !== 200) {
+        callback(Error(`Status Code ${res.statusCode} when fetching IP: ${body}`), null);
+        return;
+      }
+  
+      const myIP = JSON.parse(body).ip;
+      callback(null, myIP);
     })
 
   }
